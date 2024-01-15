10c5c73832f8f054b560f9fcb844e2d8f565cf63
hxrda <henein.roda@gmail.com>
2024-01-15 17:58:24 +0200

    Exercise complete

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Alert, TextInput } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	//States:
+	const [message, setMessage] = useState(""); //initialized as empty string
+
+	//Functions:
+	const showAlert = () => {
+		Alert.alert("Hello", "You typed: " + message); //"alert" = method of Alert component
+		console.log("Hello console");
+	};
+
+	//Rendering:
+	return (
+		<View style={styles.container}>
+			<TextInput
+				style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+				onChangeText={(text) => setMessage(text)}
+			/>
+			<Button color="green" title="Press me" onPress={showAlert} />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
